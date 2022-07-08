2e439ca0dc5495e7cf3b90fa1c4d85795f5563d5
iBelow <8112733+iBelow@users.noreply.github.com>
2022-07-08 21:50:07 +0300

    updated depends

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/wladimir/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/wladimir/Documents/projects/photocheckin/camera_camera/example"
+export "FLUTTER_ROOT=C:\tools\flutter-3"
+export "FLUTTER_APPLICATION_PATH=D:\dev\Work\camera_app\frontend\pplus_camera\packages\camera_camera\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
